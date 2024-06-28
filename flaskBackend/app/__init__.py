824751670e812439d31470bbb6c046df5a25151f
Ayoralol <121880251+Ayoralol@users.noreply.github.com>
2024-06-28 13:33:47 +1000

    added initial config of JWT Auth

--- flaskBackend/app/__init__.py
+++ flaskBackend/app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask 
 from config import Config
+from flask_jwt_extended import JWTManager
 from .extensions import db
 from .routes.user_routes import user_bp
+
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    jwt.init_app(app)
 
     with app.app_context():
         from .models import user
--- flaskBackend/config.py
+++ flaskBackend/config.py
@@ -1,7 +1,12 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'secret-jwt-key'
+    JWT_ACCESS_TOKEN_EXPIRES = int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES', 3600))
